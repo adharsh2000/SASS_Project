c518d614df7d3075abc08979aa2e64ce1a409cc6
adharsh <adharshmr001@gmail.com>
2025-03-04 11:59:54 +0530

    sub nav created

--- client/src/app/(home)/me/layout.tsx
+++ client/src/app/(home)/me/layout.tsx
@@ -24,7 +24,7 @@ export default function PageLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 sticky top-0 bg-background z-10">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
@@ -43,6 +43,20 @@ export default function PageLayout({
             </Breadcrumb>
           </div>
         </header>
+        <div className="w-full px-4 py-3 flex gap-3 text-sm">
+          <span className="opacity-50 hover:opacity-100 transition-opacity pb-1 border-b-2 border-primary font-semibold">
+            <a href="#">Home</a>
+          </span>
+          <span>
+            <a href="#">Home</a>
+          </span>
+          <span>
+            <a href="#">Home</a>
+          </span>
+          <span>
+            <a href="#">Home</a>
+          </span>
+        </div>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
       </SidebarInset>
     </SidebarProvider>
